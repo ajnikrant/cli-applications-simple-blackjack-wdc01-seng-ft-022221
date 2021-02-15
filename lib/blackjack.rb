974f44d17fa6d66f1eb7548924afbe5ed88629b6
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-15 18:40:07 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -8,8 +8,7 @@ end
 
 def display_card_total(card_total)
   card_total= deal_card + deal_card
-  p card_total
-  puts "Your cards add up to #{card_total}"
+  p "Your cards add up to #{card_total}"
 end
 
 def prompt_user
