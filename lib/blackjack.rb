71d1fe3c73f09cde69517dea4aec0f00c2934d28
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-15 19:17:17 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -25,12 +25,12 @@ def end_game(card_total)
   end
 end
 
-def initial_round()
+def initial_round
   deal_card
   deal_card
   card_total=deal_card + deal_card
     return card_total
-      p display_card_total(card_total)
+      puts display_card_total(card_total)
 end
 
 def hit?
