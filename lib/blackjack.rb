3971ace20c5d53861b81fcc3efccc0fe56527c79
Amanda Nikrant <ajnikrant@gmail.com>
2021-02-15 18:54:32 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -19,8 +19,10 @@ def get_user_input
   gets.chomp
 end
 
-def end_game
-  # code #end_game here
+def end_game(number)
+  if number>21
+    puts "Sorry, you hit #{card_total}. Thanks for playing!"
+  end
 end
 
 def initial_round
